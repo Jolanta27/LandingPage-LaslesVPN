13b6dabad4dd54cfe5957788e22e3c31a5816c0c
Jolanta Galczynska <jolanta.galczynska@gmail.com>
2024-01-11 09:50:32 +0100

    added component to app

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
 import './Footer.css';
+import logo from '../Logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF } from  '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from  '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from  '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
     return (
-        <>
+        <footer>
         <div className="footer-main-section">
             <div className='subscription-main'>
                 <div className='subscription-text'>
@@ -14,7 +20,46 @@ const Footer = () => {
                 </div>
             </div>
         </div>
-        </>
+        <div className='footer-main-section'>
+            <div className='footer-logos'>
+                <img src={logo} alt="logo" />
+                <p>LaslesVPN is a private virtual network that has unique features and has high security.</p>
+                <FontAwesomeIcon icon={faFacebookF} />
+                <FontAwesomeIcon icon={faTwitter} />
+                <FontAwesomeIcon icon={faInstagram} />
+                <p>©{currentYear}Lasles<span>VPN</span></p>
+            </div>
+            <div className='footer-links-section'>
+            <div className='footer-links-main'>
+                <h4>Product</h4>
+                <ul>
+                    <li>Download</li>
+                    <li>Pricing</li>
+                    <li>Locations</li>
+                    <li>Server</li>
+                    <li>Countries</li>
+                    <li>Blog</li>
+                </ul>
+            </div>
+            <div>
+                <h4>Engage</h4>
+                <ul>
+                    <li>LaslesVPN?</li>
+                    <li>FAQ</li>
+                    <li>Tutorials</li>
+                    <li>About Us</li>
+                    <li>Privacy Policy</li>
+                    <li>Terms of Service</li>
+                </ul>
+            </div>
+            <div>
+                <h4>Earn Money</h4>
+                <li>Affiliate</li>
+                <li>Become Partner</li>
+            </div>
+        </div>
+        </div>
+        </footer>
     )
 }
 export default Footer;
