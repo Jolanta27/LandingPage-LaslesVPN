d2f458548eb566d5a7796183b83b934dae48bfb2
Jolanta Galczynska <jolanta.galczynska@gmail.com>
2024-01-16 11:05:51 +0100

    changes in mobile version of navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles/Header.css";
 import Hamburger from 'hamburger-react'
-import { useState }  from "react";
+import { useState } from "react";
 
 const NavItem = ({ link, text}) => {
     return (
@@ -13,40 +13,57 @@ const NavItem = ({ link, text}) => {
 
  const NavbarButton = ({ text, position}) => {
   return (
-    <button className={`navbar-button button-${position}`}>{text}</button> 
+    <button className={`navbar-button button-${position}`}>{text}</button>
   )
  }
 
- const HamburgerButton = ({isOpen, toggle}) => {
+/* const HamburgerButton = ({isOpen, toggle}) => {
     return (
-        <Hamburger toggled={isOpen} size= {20} toggle={toggle}/>
+        <Hamburger toggled={isOpen} size= {25} toggle={toggle}/>
     )
- };
+ };*/
 
 const Header = () => {
     const [isOpen, setOpen] = useState(false);
-    return (
-        <>
-        <nav>
-             <HamburgerButton isOpen={isOpen} toggle={() => setOpen(!isOpen)}/>
-             {isOpen && (
-                <>
-            <img src="logo.JPG" id="logo" alt="logo" />
-            <ul className="navbar-list" onClick={() => setOpen((prev) => !prev)}>
-            <NavItem link="/" text="About" />
-            <NavItem link="/" text="Features" />
-            <NavItem link="/" text="Pricing" />
-            <NavItem link="/" text="Testimonials" />
-            <NavItem link="/" text="Help" />
-            </ul>
-            <div className="navbar-buttons">
-            <NavbarButton text="Sign In" position="left"/>
-            <NavbarButton text="Sign Up" position="right"/>
-            </div>
-            </>
-             )}    
-        </nav>
-        </>
-    )
-} 
-export default Header;
+
+    const toggleMenu = () => {
+        setOpen(!isOpen);
+    };
+    /*const renderMenu = isOpen ? (
+        <> 
+                <ul>
+                <NavItem link="/" text="About" />
+                <NavItem link="/" text="Features" />
+                <NavItem link="/" text="Pricing" />
+                <NavItem link="/" text="Testimonials" />
+                <NavItem link="/" text="Help" />
+                </ul>
+                <div className="navbar-buttons">
+                <NavbarButton text="Sign In" position="left"/>
+                <NavbarButton text="Sign Up" position="right"/>
+                </div>
+                </>
+*/
+            return (
+                <div >
+                <nav className="navbar-list">
+                    <img src="logo.JPG" id="logo" alt="logo" />
+                    <div className="navbar-mobile navbar-laptop">
+                    <Hamburger toggled={isOpen} size={25} toggle={toggleMenu} />
+                    <ul className={`menu-items $ {isOpen ? "show" : ""}`}>
+                    <NavItem link="/" text="About" />
+                    <NavItem link="/" text="Features" />
+                    <NavItem link="/" text="Pricing" />
+                    <NavItem link="/" text="Testimonials" />
+                    <NavItem link="/" text="Help" />
+                    </ul>
+                    <div className="navbar-buttons">
+                    <NavbarButton text="Sign In" position="left"/>
+                    <NavbarButton text="Sign Up" position="right"/>
+                    </div>
+                    </div>    
+                </nav>
+                </div>
+            )
+        } 
+        export default Header;
