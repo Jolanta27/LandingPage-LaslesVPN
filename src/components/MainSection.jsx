c8d772c05e74b21feceb8fb809622fe9cc9c3a1f
Jolanta Galczynska <jolanta.galczynska@gmail.com>
2024-01-22 14:36:42 +0100

    fixed mobile version of main section

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -5,19 +5,19 @@ import picture from '../../src/pictures/header-illustration.svg';
 const MainSection = () => {
     return (
         <div className='main-section'>
-        <div className='left-section'>
-        <h1>
-            Want anything to be easy with <span className='brand-name'>LaslesVPN.</span>
+        <div>
+        <h1 className='main-section-main'>
+            Want anything to be easy with <span>LaslesVPN.</span>
         </h1>
         <p>
             Provide a network for all your needs with ease and fun using LaslesVPN discover interesting features from us.
         </p>
-        <div className='centered-button'>
-        <button>Get Started</button>
+        <div>
+        <button className='started-btn'>Get Started</button>
         </div>
     </div>
-    <div className='image-section'>
-    <img src={picture} className='main-picture' alt="The guy working" />
+    <div className='parent-img'>
+    <img src={picture} alt="The guy working" />
     </div>
     </div>
     );
