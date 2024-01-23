305ecddb2584eb09b93910c9d08981ea5fba9c9b
Jolanta Galczynska <jolanta.galczynska@gmail.com>
2024-01-23 07:09:39 +0100

    added responsiveness to map section

--- src/components/MapSection.jsx
+++ src/components/MapSection.jsx
@@ -3,13 +3,15 @@ import './styles/MapSection.css';
 
 const MapSection = () => {
     return (
-    <div className='map-section-main'>
+    <div className='map-section'>
+        <div className='text-container'>
         <h2>
             Huge Global Network of Fast VPN
         </h2>
         <p>
             See <span>LaslesVPN</span> everywhere to make it easier for you when you move locations.
         </p>
+        </div>
         <img src={map} alt="map" />
      </div>
     )
