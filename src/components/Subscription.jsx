286a890fcbdf02c6590b795124f7bade88650055
Jolanta Galczynska <jolanta.galczynska@gmail.com>
2024-01-29 14:19:28 +0100

    add newsletter form and some validation

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import './styles/Subscription.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import './styles/Subscription.css';
 
 const SubscriptionForm = () => {
 
@@ -12,64 +14,67 @@ const SubscriptionForm = () => {
         acceptTerms: false,
     };
 
-    const handleSubmit = (values) => {
-        console.log(values);
+    const handleSubmit = (values, {resetForm}) => {
+        resetForm();
     };
 
     return (
         <Formik initialValues={initialValues} onSubmit={handleSubmit} className="my-form-container">
-            <Form className='subscription-content'>
-            <FontAwesomeIcon icon="fa-regular fa-envelope" className='envelope-icon'/>
-               <h1>Newsletter</h1>
-                <p>Stay up to date with our latest news and products</p>
-                <div>
-                    <label htmlFor="email">
-                        Email:
-                    </label>
-                    <Field type="email" id="email" name="email" />
-                    <ErrorMessage name="email" component="div" />
-                </div>
-
-                <div>
-                    <label>
-                        Topics of Interest:
-                    </label>
+            {() => (
+                <Form className='subscription-content' noValidate type="email">
+                <FontAwesomeIcon icon={faEnvelope} className='envelope-icon'/>
+                   <h1>Newsletter</h1>
+                    <p>Stay up to date with our latest news and products</p>
+                    <div className='email-form'>
+                        <label htmlFor="email">
+                            Email:
+                        </label>
+                        <Field type="email" id="email" name="email" className="email-field"/>
+                        <ErrorMessage name="email" component="div" />
+                    </div>
+    
+                    <div className='choose-points'>
+                        <label>
+                            Topics of Interest:
+                        </label>
+                        <div>
+                            <label>
+                                <Field type="checkbox" name="topics" value="topic1" />
+                                Technology
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <Field type="checkbox" name="topics" value="topic2" />
+                                Business
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <Field type="checkbox" name="topics" value="topic3" />
+                                Entertainment
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <Field type="checkbox" name="topics" value="topic3" />
+                                Others
+                            </label>
+                        </div>
+                    </div>
+    
                     <div>
                         <label>
-                            <Field type="checkbox" name="topics" value="topic1" />
-                            Technology
+                            <Field type="checkbox" name="acceptTerms" />
+                            I accept the terms and conditions
                         </label>
+                        <ErrorMessage name="acceptTerms" component="div" />
                     </div>
-                    <div>
-                        <label>
-                            <Field type="checkbox" name="topics" value="topic2" />
-                            Business
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            <Field type="checkbox" name="topics" value="topic3" />
-                            Entertainment
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            <Field type="checkbox" name="topics" value="topic3" />
-                            Others
-                        </label>
-                    </div>
-                </div>
-
-                <div>
-                    <label>
-                        <Field type="checkbox" name="acceptTerms" />
-                        I accept the terms and conditions
-                    </label>
-                    <ErrorMessage name="acceptTerms" component="div" />
-                </div>
-
-                <button type="submit">Subscribe</button>
-            </Form>
+    
+                    <button type="submit">Subscribe</button>
+                </Form>
+            )}
+            
         </Formik>
     );
 };
